3748660dba9d7114a6add7cc31323af31b4d5695
janmesh Singh <Janmesh.js@gmail.com>
2023-04-13 22:21:20 +0530

    update exp

--- src/constants/index.js
+++ src/constants/index.js
@@ -136,7 +136,7 @@ import {
       iconBg: "#E6DEDD",
       date: "Jan 2021 - Feb 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Students for exploration and development of space",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
